8bf98aa06b37b22f56fc610ddbfcac1d8e4c2a37
ye-yu <rafolwen98@gmail.com>
2026-09-24 03:15:13 +0800

    feat: enhance prompt utility with improved selection and caching logic

--- prompt.util.ts
+++ prompt.util.ts
@@ -2,11 +2,12 @@ import { confirm, search } from '@inquirer/prompts'
 import { readJsonSync, mkdirSync, writeJsonSync } from './fs.util.ts';
 import FuzzySearch from 'fuzzy-search';
 import path from 'path';
+import { green } from './style.util.ts';
 
 const cacheDir = "prompt-cache"
 const cacheJson = 'cache.json'
 const cacheFullPath = path.join(cacheDir, cacheJson)
-mkdirSync(import.meta.url, cacheFullPath)
+mkdirSync(import.meta.url, cacheDir)
 
 const responseCache = {} as Record<string, any>
 function constructCacheKey(message: string) {
@@ -36,19 +37,39 @@ type Mapper<T> = (item: T) => {
 }
 
 const jsonMapper: Mapper<any> = (json: any) => ({
-  name: JSON.stringify(json)
+  name: typeof json === "string"
+    || typeof json === "number"
+    || typeof json === "boolean" ? `${json}`
+    : JSON.stringify(json)
 })
 
-export async function simpleSelect<T extends object>(message: string, items: T[], mapper = jsonMapper) {
-  const mappedOptions = items.map((value) => ({ value, stringified: JSON.stringify(value), ...mapper(value) }))
+export async function simpleSelect<T>(message: string, items: T[], mapper = jsonMapper) {
+  const mappedOptions = items.map((value, index) => ({ 
+    value: { value, index },
+    stringified: JSON.stringify(value),
+    ...mapper(value),
+   }))
   const searcher = new FuzzySearch(mappedOptions, ['name', 'description', 'stringified'], { caseSensitive: false })
   const cacheKey = constructCacheKey(message)
   const defaultValue = responseCache[cacheKey]
   const response = await search({
     message,
-    source: searcher.search.bind(searcher),
-    default: defaultValue,
+    source: search => searcher.search(search),
+    default: mappedOptions[defaultValue]?.value,
   })
-  saveResponse(cacheKey, response)
-  return response
+  saveResponse(cacheKey, response.index)
+  return response.value
+}
+
+/**
+ * node -p -e "import('./prompt.util.ts').then(e => e.testSimpleSelect())"
+ */
+export async function testSimpleSelect() {
+  const types = ["string", "object"]
+  const selectedType = await simpleSelect("Select test type:", types)
+  const fruits = ['Apple', 'Oranges', 'Pineapple']
+  const options = selectedType === "object" ? fruits.map(e => ({ fruit: e })) : fruits
+  const selectedFruits = await simpleSelect("Select your fruits:", options as any)
+  console.log("You selected", green(jsonMapper(selectedFruits).name))
+  return selectedFruits
 }
